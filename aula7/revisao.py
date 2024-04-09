8ac4e647eb83806e2890cb7a46f8e104b4d6da06
Kayky Ferreira <ferreirakayky79@gmail.com>
2024-04-09 14:37:54 -0300

    automacao

---
+++ aula7/revisao.py
@@ -0,0 +1,24 @@
+
+import pyautogui, pyscreeze
+
+#mover 
+#pyautogui.moveTo(1112, 1052, duration=0.5)
+#pyautogui.moveRel(100,0, duration=0.5)
+#pyautogui.moveRel(0,-100, duration=0.5)
+
+#arrastar
+#pyautogui.moveTo(257,128, duration=0.5)
+#pyautogui.dragTo(27,19, duration=0.5)
+
+#clicar
+#pyautogui.click(57, 17, duration=0.5)
+#pyautogui.click(544, 481, duration=0.5,clicks=2)
+
+#Localizar um item na tela
+btnXY = pyautogui.locateCenterOnScreen('./aula7/btn_edit.png')
+pyautogui.click(btnXY,duration=0.5)
+
+#rolar bolinha
+
+
+
